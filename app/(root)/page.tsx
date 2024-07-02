b5e61de98a2f21198dece94930aa19d7c5b3e244
Nick Frazao <nicolas_frazao@hotmail.com>
2024-07-02 14:37:32 -0300

    tailwind classes added

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="home">
+      <div>
+        <header>
+          Header Box
+          Total Balance Box
+        </header>
+        Recent Transations
+      </div>
+        Right Side Bar
+    </main>
+  );
+}
