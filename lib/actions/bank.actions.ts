9a4604ddc467d7031ddb14caee5fd8a7c61dfe29
Nick Frazao <nicolas_frazao@hotmail.com>
2024-07-08 18:36:46 -0300

    refactored bank.actions

--- lib/actions/bank.actions.ts
+++ lib/actions/bank.actions.ts
@@ -30,7 +30,7 @@ export const getAccounts = async ( { userId }: getAccountsProps ) =>
 
         const accountData = await accountsResponse.data.accounts[ 0 ];
 
-        const instituition = await getInstitution( {
+        const institution = await getInstitution( {
           institutionId: accountsResponse.data.item.institution_id!,
         } );
         
@@ -38,7 +38,7 @@ export const getAccounts = async ( { userId }: getAccountsProps ) =>
           id: accountData.account_id,
           availableBalance: accountData.balances.available!,
           currentBalance: accountData.balances.current!,
-          instituitionId: instituition.institution_id,
+          institutionId: institution.institution_id,
           name: accountData.name,
           officialName: accountData.official_name,
           mask: accountData.mask!,
@@ -68,8 +68,117 @@ export const getAccounts = async ( { userId }: getAccountsProps ) =>
 export const getAccount = async ( { appwriteItemId }: getAccountProps) => {
   
   try {
-    const bank = await getBank( {documentId: appwriteItemId} );
-  } catch (error) {
+    const bank = await getBank( { documentId: appwriteItemId } );
     
+    const accountsResponse = await plaidClient.accountsGet( {
+      access_token: bank.accessToken
+    } );
+
+    const accountData = accountsResponse.data.accounts[0];
+
+    const transferTransactionsData = await getTransactionsByBankId( {
+      bankId: bank.$id,
+    } );
+    
+    const transferTransaction = transferTransactionsData.document.map(
+      ( transferData: Transaction ) => ( {
+        id: transferData.$id,
+        name: transferData.name!,
+        amount: transferData.amount!,
+        date: transferData.$createdAt,
+        paymentChannel: transferData.channel,
+        category: transferData.category,
+        type: transferData.senderBankId === bank.$id ? 'debit' : 'credit',
+      } )
+    );
+
+    const institution = await getInstitution( {
+      institutionId: accountsResponse.data.item.institution_id!
+    } );
+    
+    const transactions = await getTransactions({
+      accessToken: bank?.accessToken
+    } );
+    
+    const account = {
+      id: accountData.account_id,
+      availableBalance: accountData.balances.available!,
+      currentBalance: accountData.balances.current!,
+      institutionId: institution.institution_id,
+      name: accountData.name,
+      officialName: accountData.official_name,
+      mask: accountData.mask!,
+      type: accountData.type as string,
+      subtype: accountData.subtype! as string,
+      appwriteItemId: bank.$id,
+    };
+
+    const allTransaction = [ ...transactions, ...transferTransaction ].sort(
+      ( a, b ) => new Date( b.date ).getTime() - new Date( a.date ).getTime()
+    );
+
+    return parseStringify( {
+      data: account,
+      transactions: allTransaction
+    });
+
+  } catch (error) {
+    console.error("An error occurred while getting the account", error)
   }
- };
+};
+ 
+export const getInstitution = async ( {
+  institutionId,
+}: getInstitutionProps ) =>
+{ 
+  try {
+    const institutionResponse = await plaidClient.institutionsGetById( {
+      institution_id: institutionId,
+      country_codes: ["US"] as CountryCode[],
+    } );
+    
+    const intitution = institutionResponse.data.institution;
+
+    return parseStringify( intitution );
+  } catch (error) {
+    console.error( "An error occurred while getting the accounts:", error );
+  }
+};
+
+export const getTransactions = async (
+  {accessToken} : getTransactionsProps
+) =>
+{ 
+  let hasMore = true;
+  let transactions: any = [];
+
+  try {
+    while ( hasMore )
+    {
+      const response = await plaidClient.transactionsSync( {
+        access_token: accessToken,
+      } );
+      
+      const data = response.data;
+
+      transactions = response.data.added.map( ( transaction ) => ( {
+        id: transaction.transaction_id,
+        name: transaction.name,
+        paymentChannel: transaction.payment_channel,
+        type: transaction.payment_channel,
+        accountId: transaction.account_id,
+        amount: transaction.amount,
+        pending: transaction.pending,
+        category: transaction.category ? transaction.category[ 0 ] : "",
+        date: transaction.date,
+        image: transaction.logo_url
+      } ) );
+      
+      hasMore = data.has_more;
+    }
+
+    return parseStringify( transactions );
+  } catch (error) {
+    console.error("An error has occurred while getting the accounts", error)
+  }
+};
